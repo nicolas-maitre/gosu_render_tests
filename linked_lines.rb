9c1425f05f862c5c28b05adc0ae97693f63cfd34
Nicolas Maitre <47384878+nicolas-maitre@users.noreply.github.com>
2021-01-05 00:43:12 +0100

    bouncy_bouce

--- linked_lines.rb
+++ linked_lines.rb
@@ -1,16 +1,17 @@
 require 'gosu'
 class LinkedLines < Gosu::Window
-    CIRCLE_RADIUS = 200
     def initialize
         @points = []
         @selected_point = nil
-        super 1280, 720
+        @autorotate = false
+        super 1920, 1080, fullscreen: true
 
         #gen circle
-        update_circle(10)
+        update_circle(10, 200)
     end
     def update
         @points << {x: mouse_x, y: mouse_y} if button_down? Gosu::KB_SPACE
+        update_circle if @autorotate
     end
     def draw
         those_points = button_down?(Gosu::MS_RIGHT) ? @points + [{x:mouse_x, y:mouse_y}] : @points
@@ -29,29 +30,38 @@ class LinkedLines < Gosu::Window
         end
     end
     def button_down button
+        exit if button == Gosu::KB_ESCAPE
+        if(button == Gosu::KB_P)
+            @autorotate^=true
+            return
+        end
         if(button == Gosu::KB_R)
             @points.clear
             @circle_node_counts = 10
             return
         end
         if(button == Gosu::KB_UP)
-            update_circle(@circle_node_counts + 1)
+            update_circle(@circle_node_counts+1)
             return
         end
         if(button == Gosu::KB_DOWN)
-            update_circle(@circle_node_counts - 1)
+            update_circle(@circle_node_counts-1)
             return
         end
         return if [Gosu::MS_RIGHT, Gosu::KB_SPACE].include? button
         @points << {x: mouse_x, y: mouse_y}
     end
-    def update_circle(count)
+    H_SCALE_TIME = 0.8
+    V_SCALE_TIME = 0.9
+    def update_circle(count=@circle_node_counts, radius=@circle_radius)
+        time = @autorotate ? Time.now.to_f : 0
         @circle_node_counts = count
+        @circle_radius = radius
         spacing = 2 * Math::PI / @circle_node_counts
         @points = 0.upto(@circle_node_counts - 1).map do |index|
-            angle = spacing * index
-            x_pos = (Math.sin(angle) + 1)*CIRCLE_RADIUS
-            y_pos = (Math.cos(angle) + 1)*CIRCLE_RADIUS
+            angle = spacing * index + time
+            x_pos = (Math.sin(angle) + 1)*@circle_radius*(Math.sin(time / H_SCALE_TIME) + 1.5)
+            y_pos = (Math.cos(angle) + 1)*@circle_radius*(Math.sin(time / V_SCALE_TIME) + 1.5)
             {x: x_pos + 200, y: y_pos + 100}
         end
     end
